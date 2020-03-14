0fdf0af42845fbab877ab33f81630920fe3f3fc2
Tobenna-KA <kabanofor@yahoo.com>
2020-03-15 01:26:50 +0300

    minor changes

--- server.py
+++ server.py
@@ -29,24 +29,24 @@ def api_prediction():
     prediction = capsule_net.capsule_prediction(image_path)
     if not os.path.isdir('predicted_images'):
         os.mkdir('predicted_images', 0o777)
-    if not os.path.isdir('predicted_images/' + prediction['disease']):
-        os.mkdir('predicted_images/' + prediction['disease'])
+    if not os.path.isdir('static/predicted_images/' + prediction['disease']):
+        os.mkdir('static/predicted_images/' + prediction['disease'])
 
     # move the file
-    shutil.move(image_path, 'predicted_images/' + prediction['disease'] + "/" + os.path.basename(image_path))
+    shutil.move(image_path, 'static/predicted_images/' + prediction['disease'] + "/" + os.path.basename(image_path))
     js = json.dumps({
         'disease': prediction['disease'],
         'accuracy': 92,
-        'image_path': 'predicted_images/' + prediction['disease'] + "/" + os.path.basename(image_path),
+        'image_path': 'static/predicted_images/' + prediction['disease'] + "/" + os.path.basename(image_path),
         'success': True
     })
     resp = Response(js, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/articles/<articleid>')
-def api_article(articleid):
-    return 'You are reading ' + articleid
+@app.route('/articles/<article_id>')
+def api_article(article_id):
+    return 'You are reading ' + article_id
 
 
 if __name__ == '__main__':
